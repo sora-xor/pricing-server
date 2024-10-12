2ce8686110bfd801617ca4475f5a57d4f1ef75c3
f33r0 <95526886+f33r0@users.noreply.github.com>
2024-10-12 10:15:44 +0300

    revert-begin-to-1

--- run_node_processing.py
+++ run_node_processing.py
@@ -255,7 +255,7 @@ def get_event_param(event, param_idx):
     return get_value(attribute)
 
 
-async def async_main(async_session, begin=109052, clean=False, silent=False):
+async def async_main(async_session, begin=1, clean=False, silent=False):
     # if clean:
     #     async with db.engine.begin() as conn:
     #         await conn.run_sync(models.Base.metadata.drop_all)
